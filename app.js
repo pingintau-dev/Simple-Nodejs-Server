b39b27994da18a4cd6cb4e1e5fb7777487836d1a
pingintau <64389449+pingintau-dev@users.noreply.github.com>
2026-05-18 13:46:30 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to AWS tutorial by Faisal Tarru");
+})
+
+app.get("/product", (req, res) => {
+  res.send("This is Product Page");
+})
+
+app.get("/health", (req, res) => {
+  res.send("Everything is OK");
+})
+
+app.listen(8080, () => console.log("Sever is listening on Port 8080"))
